36687a2142df23049e0265f69b074967d5dd3d1b
bcasalvieri <bmcasalvieri@gmail.com>
2020-04-02 11:27:35 -0400

    adding bean validations & exception handler

---
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name("Brad")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+
+    }
+}
